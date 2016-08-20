afe99e5591fdd130f1e9f40c00b9ebf43dd08bc5
Tom Pratt <tom.pratt@live.com>
2016-08-20 18:37:33 +0200

    Adding projects to solution

---
+++ Sylapse.MainThreadDispatch/Dispatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace Sylapse.MainThreadDispatch
+{
+    public delegate bool SetFunc<T>(Expression<Func<T>> propertyExpression, ref T field, T newValue);
+
+    public static class DispatchExtensions
+    {
+        public static bool Dispatch<T>(this ObservableObject observableObject, SetFunc<T> set, Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+#if PORTABLE
+            throw new PlatformNotSupportedException("Please visit the Sylapse Github to find out about supporting other platforms");
+#else
+            return set(propertyExpression, ref field, newValue);
+#endif
+        }
+    }
+}
