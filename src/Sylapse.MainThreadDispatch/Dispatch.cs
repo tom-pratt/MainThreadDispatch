ff1a3b23997fc2c3d6340e31aeccae6a2c40ebcf
Tom Pratt <tom.pratt@live.com>
2016-08-20 22:13:14 +0200

    Added DispatchSet methods to mimic the MvvmLight Set method

--- src/Sylapse.MainThreadDispatch/Dispatch.cs
+++ src/Sylapse.MainThreadDispatch/Dispatch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using System.Text;
 using GalaSoft.MvvmLight;
 
@@ -14,10 +15,57 @@ namespace Sylapse.MainThreadDispatch
         public static void Dispatch(this ObservableObject observableObject, Action action)
         {
 #if PORTABLE
-            throw new PlatformNotSupportedException("Make sure MainThreadDispatcher is added to your platform project. Otherwise, please visit the Sylapse.MainThreadDispatch Github page to find out about supporting other platforms");
+            throw NotSupported();
 #else
             MainThreadDispatcher.Instance.Execute(action);
 #endif
+        }
+
+        public static bool DispatchSet<T>(this ObservableObject observableObject, Expression<Func<T>> propertyExpression, ref T field, T newValue)
+        {
+#if PORTABLE
+            throw NotSupported();
+#else
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+
+            MainThreadDispatcher.Instance.Execute(() => observableObject.RaisePropertyChanged(propertyExpression));
+            return true;
+#endif
+        }
+
+        public static bool DispatchSet<T>(this ObservableObject observableObject, string propertyName, ref T field, T newValue)
+        {
+#if PORTABLE
+            throw NotSupported();
+#else
+            if (EqualityComparer<T>.Default.Equals(field, newValue))
+            {
+                return false;
+            }
+
+            field = newValue;
+
+            MainThreadDispatcher.Instance.Execute(() => observableObject.RaisePropertyChanged(propertyName));
+            return true;
+#endif
+        }
+        public static bool DispatchSet<T>(this ObservableObject observableObject, ref T field, T newValue, [CallerMemberName] string propertyName = null)
+        {
+#if PORTABLE
+            throw NotSupported();
+#else
+            return observableObject.DispatchSet(propertyName, ref field, newValue);
+#endif
+        }
+
+        static Exception NotSupported()
+        {
+            return new PlatformNotSupportedException("Make sure MainThreadDispatcher is added to your platform project. Otherwise, please visit the Sylapse.MainThreadDispatch Github page to find out about supporting other platforms");
         }
     }
 }
