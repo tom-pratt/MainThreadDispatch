ca5897e3bbef2cb330d9d3d3804ba350a488e5af
Tom Pratt <tom.pratt@live.com>
2016-08-27 16:29:34 +0200

    Added UWP version

---
+++ src/Sylapse.MainThreadDispatch.UWP/MainThreadDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+
+namespace Sylapse.MainThreadDispatch
+{
+    public class MainThreadDispatcher
+    {
+        public static MainThreadDispatcher Instance = new MainThreadDispatcher();
+
+        public MainThreadDispatcher()
+        {
+        }
+
+        public async void Execute(Action action)
+        {
+            await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                action();
+            });
+        }
+    }
+}
---
+++ src/Sylapse.MainThreadDispatch/MainThreadDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sylapse.MainThreadDispatch
+{
+    public class MainThreadDispatcher
+    {
+        public static MainThreadDispatcher Instance = new MainThreadDispatcher();
+
+        public MainThreadDispatcher()
+        {
+        }
+
+        public void Execute(Action action)
+        {
+            throw new PlatformNotSupportedException("Make sure Sylapse.MainThreadDispatcher is added to your platform project. If it is then you are probably using an unsupported platform, please visit https://github.com/Sylapse/MainThreadDispatch to find out about supporting other platforms");
+        }
+    }
+}
