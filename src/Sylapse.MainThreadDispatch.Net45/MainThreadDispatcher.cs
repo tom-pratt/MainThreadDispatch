c33cef89c253d42ee213112e5d6003c6630e52e6
Tom Pratt <tom.pratt@live.com>
2016-08-31 21:26:22 +0200

    Added .Net45 project

---
+++ src/Sylapse.MainThreadDispatch.Net45/MainThreadDispatcher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sylapse.MainThreadDispatch
+{
+    public class MainThreadDispatcher
+    {
+        public static MainThreadDispatcher Instance = new MainThreadDispatcher();
+
+        public MainThreadDispatcher()
+        {
+        }
+
+        public void Execute(Action action)
+        {
+            action();
+        }
+    }
+}
